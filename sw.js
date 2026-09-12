530701abc0b9b384d6019ec65f0616e2f86aa816
dapis88 <suoyushan@gmail.com>
2026-09-12 18:33:11 +0800

    修复时间选择

--- sw.js
+++ sw.js
@@ -1,3 +1,4 @@
-const CACHE='bigp-log-v1';
-self.addEventListener('install', e => e.waitUntil(caches.open(CACHE).then(c => c.addAll(['./','./index.html','./style.css','./app.js','./manifest.webmanifest','./icon.svg']))));
+const CACHE='bigp-log-v2';
+self.addEventListener('install', e => e.waitUntil(caches.open(CACHE).then(c => c.addAll(['./','./index.html','./style.css','./app.js','./manifest.webmanifest','./icon.svg'])).then(()=>self.skipWaiting())));
+self.addEventListener('activate', e => e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch', e => e.respondWith(caches.match(e.request).then(r => r || fetch(e.request))));
